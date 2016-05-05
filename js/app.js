a5456321bd40e8b05ad8fc51a31188b1580716c5
nikthequik <quik.the.nick@gmail.com>
2016-05-05 15:20:58 -0600

    -add numGen and newGame functions -numGen works

--- js/app.js
+++ js/app.js
@@ -12,6 +12,21 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+	function numGen(){
+		var num = Math.random() * 100;
+		num = Math.round(num);
+		return num;
+  	};
+
+  	/*--- Start a new game ---*/
+  	function newGame() {
+  		var secretNum = numGen();
+  		console.log(secretNum);
+  	};
+
+  	newGame();
+
+
 });
 
 

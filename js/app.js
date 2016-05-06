0522eccd324fe173aea4055e567d2dd647e05e50
nikthequik <quik.the.nick@gmail.com>
2016-05-05 20:04:19 -0600

    -completed game

--- js/app.js
+++ js/app.js
@@ -17,7 +17,11 @@ $(document).ready(function(){
   	/*---Event Listeners---*/
   	$('#guessButton').click(function(){
   		feedback();
-  	})
+  	});
+
+  	$('.new').click(function(){
+  		newGame();
+  	});
 
   	/*---Custom Functions---*/
 	function numGen(){
@@ -26,48 +30,62 @@ $(document).ready(function(){
 		return num;
   	};
 
-  	function feedback(int) {
+  	function feedback() {
   		var guess = $('#userGuess').val();
   		guess = parseInt(guess);
   		if (!guess) {
   			alert("It seems that what you entered was not a number.  Please enter a number between 1 and 100.");
   		}
+  		else if (guess > 100) {
+  			alert("The number you entered was over 100. Please enter a number between 1 and 100.");
+  		}
   		else {
+  			guessCount++;
   			var diff = Math.abs(guess - secretNum);
-  			if (diff >= 50) {
+  			if (diff === 0) {
+  				$('#feedback').text("You won in " + guessCount + " guesses!");
+  				$('#guessButton').prop('disabled', true);
+  			}
+  			else if (diff >= 50) {
   				$('#feedback').text("I can't believe how cold you are.");
-
+  				
   			}
   			else if (diff >= 40) {
   				$('#feedback').text("Pretty cold, but not arctic.");
   			}
   			else if (diff >= 30) {
   				$('#feedback').text("Luke warm.");
+  				
   			}
   			else if (diff >= 20) {
   				$('#feedback').text("You're pretty warm, but not hot.");
+  				
   			}
   			else if (diff >= 10) {
   				$('#feedback').text("You are hot.");
+  				
   			}
   			else {
   				$('#feedback').text("You are as hot as a habanero!");
+  				
   			}
+  			$('#guessList').append("<li>" + guess + "</li>");
+  			$('#count').text(guessCount);
   		}
-  	}
+  	};
 
   	/*--- Start a new game ---*/
   	function newGame() {
   		secretNum = numGen();
-  		guessCount = 0;	
+  		guessCount = 0;
+  		$('#count').text(guessCount);
+  		$('#guessList').text("");
+  		$('#feedback').text("Make your Guess!");
+  		$('#guessButton').prop('disabled', false);
+  		$('#userGuess').val("");
+
   	};
-
   	newGame();
-  	console.log(secretNum);
-  	console.log(guessCount);
-  	
-
-
 });
 
 

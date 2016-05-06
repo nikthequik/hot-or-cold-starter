443e1f3cc4b1225f1a938007d06e1b4b7e941a25
nikthequik <quik.the.nick@gmail.com>
2016-05-05 18:42:28 -0600

    -build out feedback

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
+var secretNum,
+	guessCount;
 
 $(document).ready(function(){
-	
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,19 +14,58 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	/*---Event Listeners---*/
+  	$('#guessButton').click(function(){
+  		feedback();
+  	})
+
+  	/*---Custom Functions---*/
 	function numGen(){
 		var num = Math.random() * 100;
 		num = Math.round(num);
 		return num;
   	};
 
+  	function feedback(int) {
+  		var guess = $('#userGuess').val();
+  		guess = parseInt(guess);
+  		if (!guess) {
+  			alert("It seems that what you entered was not a number.  Please enter a number between 1 and 100.");
+  		}
+  		else {
+  			var diff = Math.abs(guess - secretNum);
+  			if (diff >= 50) {
+  				$('#feedback').text("I can't believe how cold you are.");
+
+  			}
+  			else if (diff >= 40) {
+  				$('#feedback').text("Pretty cold, but not arctic.");
+  			}
+  			else if (diff >= 30) {
+  				$('#feedback').text("Luke warm.");
+  			}
+  			else if (diff >= 20) {
+  				$('#feedback').text("You're pretty warm, but not hot.");
+  			}
+  			else if (diff >= 10) {
+  				$('#feedback').text("You are hot.");
+  			}
+  			else {
+  				$('#feedback').text("You are as hot as a habanero!");
+  			}
+  		}
+  	}
+
   	/*--- Start a new game ---*/
   	function newGame() {
-  		var secretNum = numGen();
-  		console.log(secretNum);
+  		secretNum = numGen();
+  		guessCount = 0;	
   	};
 
   	newGame();
+  	console.log(secretNum);
+  	console.log(guessCount);
+  	
 
 
 });
